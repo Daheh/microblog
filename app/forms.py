ef88e805a21cd239e1eda1ebf58d867fc38daebc
Daheh <kaven.h.huard@gmail.com>
2022-03-16 02:26:32 -0400

    chap 9 pagination

--- app/forms.py
+++ app/forms.py
@@ -45,5 +45,11 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different email address.')
 
 
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+                         DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
+
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
